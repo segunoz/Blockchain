35a22835be15d851465b5097db5a782c3faac4fa
segunoz <segunoz1@gmail.com>
2021-04-01 21:14:50 +0100

    creating the block class

--- src/main.kt
+++ src/main.kt
@@ -1,9 +1,23 @@
 import com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time
 import java.util.*
+import java.time.*
+import java.math.BigInteger
+import java.security.MessageDigest
 
 fun main(args: Array<String>){
     println("This is the new Blockchain we are working on")
 
+
+}
+
+fun ByteArray.md5(): String {
+    val md = MessageDigest.getInstance("MD5")
+    return BigInteger(1, md.digest()).toString(16).padStart(32, '0')
+}
+
+fun ByteArray.sha256(): String {
+    val md = MessageDigest.getInstance("SHA-256")
+    return BigInteger(1, md.digest()).toString(16).padStart(32, '0')
 
 }
 
@@ -13,12 +27,33 @@ class Block(
     val previousHash : String,
     val data : Any){
     val hash = calculateHash()
-    val timeStamp = Date.time()
+    val timeStamp = LocalDateTime.now()
 
     fun calculateHash() : String{
         val input = (index.toString() + previousHash + timeStamp + data).toByteArray()
+        return input.md5()
 
 
     }
 
+    object Blockchain {
+        val chain = mutableListOf<Block>()
+        val latestBlock : Block
+            get() = chain.last()
+
+        init{
+            chain.add(Block(0,"0", "Genesis block"))
+        }
+
+        fun addNewBlock(block: Block){
+            if(isNewBlockValid(block)) chain.add(block)
+
+        }
+    }
+
 }
+
+
+
+
+
