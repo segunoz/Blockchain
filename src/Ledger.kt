ba7c14906cef34a60c5f9a72fd50f5af212b25b6
segunoz <segunoz1@gmail.com>
2021-04-18 16:56:10 +0100

    added a ledger class

---
+++ src/Ledger.kt
@@ -0,0 +1,8 @@
+class Ledger (){
+
+    // this class is meant to stand for any record
+
+    var debitCreditName : String = ""
+    var debitCreditID : Int = 0
+
+}
--- src/main.kt
+++ src/main.kt
@@ -1,13 +1,22 @@
-import com.sun.org.apache.xalan.internal.lib.ExsltDatetime.time
-import java.util.*
 import java.time.*
 import java.math.BigInteger
 import java.security.MessageDigest
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     println("This is the new Blockchain we are working on")
+    println("We introduce Baddo coin")
+
+    val badocoin = Blockchain
+
+    for (i in 1..15) {
+        badocoin.addNewBlock()
+    }
+
+    for (block in badocoin) {
+        println("""Data : ${block.data}  | Previous Hash : ${block.previousHash}  | Current Hash = ${block.hash} |""".trimMargin())
 
 
+    }
 }
 
 fun ByteArray.md5(): String {
@@ -21,37 +30,38 @@ fun ByteArray.sha256(): String {
 
 }
 
+    class Block(
+        val index: Int,
+        val previousHash: String,
+        val data: Any
+    ) {
+        val hash = calculateHash()
+        val timeStamp = LocalDateTime.now()
 
-class Block(
-    val index: Int,
-    val previousHash : String,
-    val data : Any){
-    val hash = calculateHash()
-    val timeStamp = LocalDateTime.now()
-
-    fun calculateHash() : String{
-        val input = (index.toString() + previousHash + timeStamp + data).toByteArray()
-        return input.md5()
-
-
-    }
-
-    object Blockchain {
-        val chain = mutableListOf<Block>()
-        val latestBlock : Block
-            get() = chain.last()
-
-        init{
-            chain.add(Block(0,"0", "Genesis block"))
-        }
-
-        fun addNewBlock(block: Block){
-            if(isNewBlockValid(block)) chain.add(block)
-
+        fun calculateHash(): String {
+            val input = (index.toString() + previousHash + timeStamp + data).toByteArray()
+            return input.cha256()
         }
     }
 
+object Blockchain {
+    val chain = mutableListOf<Block>()
+    val latestBlock: Block
+        get() = chain.last()
+
+    init {
+        chain.add(Block(0, "0", "Genesis block"))
+    }
+
+    fun addNewBlock(block: Block) {
+        if (block) {
+            chain.add(block)
+
+        }
+    }
 }
+
+
 
 
 
