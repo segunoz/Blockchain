bc954975bc93d10f1c18e00f7450ae2c7c0fbe34
segunoz <segunoz1@gmail.com>
2021-04-20 05:13:01 +0100

    trying to see the blocks as they are added  to the chain in each instance

--- src/main.kt
+++ src/main.kt
@@ -1,22 +1,28 @@
+
 import java.time.*
 import java.math.BigInteger
 import java.security.MessageDigest
+
 
 fun main(args: Array<String>) {
     println("This is the new Blockchain we are working on")
     println("We introduce Baddo coin")
 
-    val badocoin = Blockchain
+    val badoCoin = Block
+
+
 
     for (i in 1..15) {
-        badocoin.addNewBlock()
+        badoCoin.addNewBlock(Block.latestBlock)
+        //test code to see if new hash is being added to the blockchain
+        println(Block.hashCode())
     }
 
-    for (block in badocoin) {
+   /* for (block in badoCoin.chain) {
         println("""Data : ${block.data}  | Previous Hash : ${block.previousHash}  | Current Hash = ${block.hash} |""".trimMargin())
 
 
-    }
+    } */
 }
 
 fun ByteArray.md5(): String {
@@ -36,34 +42,43 @@ fun ByteArray.sha256(): String {
         val data: Any
     ) {
         val hash = calculateHash()
-        val timeStamp = LocalDateTime.now()
+        val timeStamp: LocalDateTime = LocalDateTime.now()
 
         fun calculateHash(): String {
             val input = (index.toString() + previousHash + timeStamp + data).toByteArray()
-            return input.cha256()
+            return input.sha256()
         }
-    }
 
-object Blockchain {
-    val chain = mutableListOf<Block>()
-    val latestBlock: Block
-        get() = chain.last()
+    companion object Blockchain {
+            val chain = mutableListOf<Block>()
+            val latestBlock: Block
+                get() = chain.last()
 
-    init {
-        chain.add(Block(0, "0", "Genesis block"))
-    }
+            init {
+                chain.add(Block(0, "0", "Genesis block"))
+            }
 
-    fun addNewBlock(block: Block) {
-        if (block) {
-            chain.add(block)
+
+            //to add new block to the
+            fun addNewBlock(block: Block) {
+                if (isNewBlockValid(block)) {
+                    chain.add(block)
+                }
+
+            }
 
         }
+
     }
-}
+
+
+        //to check if the block is valid.
+        fun isNewBlockValid(block: Block): Boolean {
+            return true
+        }
 
 
 
 
-
-
+        //I am having problems adding a previous hash...
 
